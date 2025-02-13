d56e4eb8ebe2e4d8bcad8bff9332843703315f57
name1essKing <dany.malkov2015@yandex.ru>
2025-02-13 03:07:59 +0300

    Добавлены файлы для игнорирования и удалены из индекса

--- GreatCalculator.client/obj/Debug/net7.0/GreatCalculator.client.AssemblyInfo.cs
+++ GreatCalculator.client/obj/Debug/net7.0/GreatCalculator.client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreatCalculator.client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951f779161e95b144f20c6fc3dd97497281f621f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eca999ecfa55e1e9bac4c4ef9d2330c2cf1d41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreatCalculator.client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreatCalculator.client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
